9861e7d854c570d6e1a1ee5d815c2893dedf3bfe
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-05 21:22:46 +0000

    Wiki-API

---
+++ app.js
@@ -0,0 +1,19 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+app.use(express.static("public"));
+
+//TODO
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
