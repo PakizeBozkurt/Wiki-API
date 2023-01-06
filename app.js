8e50daf068e3c18e2012af22abe6cf019c3ad5bc
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-06 21:09:16 +0000

    Updated

--- app.js
+++ app.js
@@ -12,7 +12,34 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-//TODO
+// Connection with MongoDB
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+// Created article Schema
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+//Article mode l
+
+const Article = mongoose.model("Article", articleSchema);
+
+//GET Route for all of the articles.
+
+app.get("/articles", function(req, res) {
+    Article.find(function(err, foundArticles) {
+        if(!err) {
+          res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+       
+    });
+});
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
