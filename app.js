f46c90be609d622b72e9c0a95c0bdc19f78c48ef
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-08 18:37:21 +0000

    POST create new

--- app.js
+++ app.js
@@ -9,18 +9,17 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-
 app.use(express.static("public"));
 
 // Connection with MongoDB
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 // Created article Schema
 
 const articleSchema = {
-    title: String,
-    content: String
+  title: String,
+  content: String,
 };
 
 //Article mode l
@@ -29,18 +28,27 @@ const Article = mongoose.model("Article", articleSchema);
 
 //GET Route for all of the articles.
 
-app.get("/articles", function(req, res) {
-    Article.find(function(err, foundArticles) {
-        if(!err) {
-          res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-       
-    });
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
 });
 
+// POST creates one new article
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.post("/articles", function (req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  newArticle.save();
+});
+
+app.listen(3011, function () {
+  console.log("Server started on port 3011");
 });
