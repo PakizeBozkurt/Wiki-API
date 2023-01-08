8b37d45f99ef90d846fa6eede89878597e7fa869
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-08 18:40:52 +0000

    Added error statment

--- app.js
+++ app.js
@@ -46,7 +46,13 @@ app.post("/articles", function (req, res) {
     content: req.body.content,
   });
 
-  newArticle.save();
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
 });
 
 app.listen(3011, function () {
